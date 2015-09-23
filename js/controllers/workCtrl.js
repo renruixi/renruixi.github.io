78b995f6cd0429d4c9f09f1e0ee5dfa8e2e3c8b6
renruixi189 <renruixi189@163.com>
2015-09-23 14:00:51 +0800

    add production

--- js/controllers/workCtrl.js
+++ js/controllers/workCtrl.js
@@ -8,20 +8,49 @@ define(["app","proChange"],function(app,proChange){
 			"btmr" :false
 		};
 
+		$scope.IntargetReverse={
+			"tpl" :false,
+			"tpr" :false,
+			"btml" :false,
+			"btmr" :false
+		};
+		
 		$scope.target={
 			"tpl" :false,
 			"tpr" :false,
 			"btml" :false,
 			"btmr" :false
 		};
+		
+		$scope.Intarget={
+			"tpl" :false,
+			"tpr" :false,
+			"btml" :false,
+			"btmr" :false
+		};
+		
 
 		$scope.changeClass=function(dir){
 
 			$scope.target[dir] = true;	
-
-			proChange.forIn($scope.targetReverse,dir);
+			
+			proChange.forIn($scope.Intarget,dir,true);
+			proChange.forIn($scope.IntargetReverse,dir,false,true);
+			proChange.forIn($scope.targetReverse,dir,false,true);
+			proChange.forIn($scope.target,dir,false,false);
 
 		};
+		
+		
+		$scope.close=function(dir){
+			
+			$scope.targetReverse[dir] = true;
+
+			proChange.forIn($scope.IntargetReverse,dir,true);
+			proChange.forIn($scope.Intarget,dir,false,true);
+			proChange.forIn($scope.targetReverse,dir,false,false);
+			proChange.forIn($scope.target,dir,false,true);
+		}
 
 
 	}]);
--- js/service/proChange.js
+++ js/service/proChange.js
@@ -1,12 +1,20 @@
 define(["app"],function(app){
 	app.factory("proChange",function(){
 
-		function proChange(obj,dir){
+		function proChange(obj,dir,bool,all){
 
-			for(var prop in obj){
-				if(obj.hasOwnProperty(prop)){
-					if(prop !== dir){
-						obj[prop] = true;
+			if(all){
+				for(var prop in obj){
+					if(obj.hasOwnProperty(prop)){
+						obj[prop] = bool;
+					}
+				}
+			}else{
+				for(var prop in obj){
+					if(obj.hasOwnProperty(prop)){
+						if(prop !== dir){
+							obj[prop] = bool;
+						}
 					}
 				}
 			}
