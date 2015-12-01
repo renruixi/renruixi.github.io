bb7cf7e1c8d0f88d6079ad03ebccaaf3d1d86249
renruixi <renruixi189@163.com>
2015-12-02 00:45:17 +0800

    controller

---
+++ newPage/js/kobe.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+
+})
