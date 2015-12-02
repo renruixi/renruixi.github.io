fa9559c82243b009b20fa7aa0576cb39e1cb4170
renruixi <renruixi189@163.com>
2015-12-02 23:29:45 +0800

    add

--- newPage/js/kobe.js
+++ newPage/js/kobe.js
@@ -1,8 +1,38 @@
 var mySwiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     loop: true,
-
+    preloadImages:false,
+    lazyLoadingOnTransitionStart : true,
     // 如果需要分页器
     pagination: '.swiper-pagination',
+    onSlideChangeStart: function (swiper) {
+        var nowSlide = $('.slide_'+swiper.activeIndex);
+        switch (swiper.activeIndex) {
+            case 1 :
+                break;
+            case 2 :
+                break;
+            case 3 :
+                nowSlide.children(".left_section").addClass("leftDown");
+                nowSlide.children(".right_section").addClass("rightUp");
+                break;
+        }
+    },
+    onSlideChangeEnd: function (swiper) {
+        var nowSlide = $('.slide_'+swiper.activeIndex);
+        switch (swiper.activeIndex) {
+            case 1 :
+                break;
+            case 2 :
+                var move = $("#kobeMove");
+                move.addClass("kobeMove kobeanimate")
+                break;
+            case 3 :
+                break;
+        }
+    }
+});
 
-})
+var animationEnd = function () {
+    
+}
