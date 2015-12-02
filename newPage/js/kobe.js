cdd1239aee309d5399c6882a10a9d4cf9c635997
renruixi <renruixi189@163.com>
2015-12-03 02:23:56 +0800

    compress picture

--- newPage/js/kobe.js
+++ newPage/js/kobe.js
@@ -6,6 +6,7 @@ var mySwiper = new Swiper('.swiper-container', {
     // 如果需要分页器
     pagination: '.swiper-pagination',
     onSlideChangeStart: function (swiper) {
+        console.log(swiper);
         var nowSlide = $('.slide_' + swiper.activeIndex);
         switch (swiper.activeIndex) {
             case 1 :
@@ -13,12 +14,13 @@ var mySwiper = new Swiper('.swiper-container', {
             case 2 :
                 break;
             case 3 :
+                var text = $('#text')
                 nowSlide.children(".left_section").addClass("leftDown");
                 nowSlide.children(".right_section").addClass("rightUp");
                 animationEnd(nowSlide.children(".right_section"), function () {
-                    $('.text').addClass("textMove");
-                    animationEnd($('.text'), function () {
-                        $(this).find(".before").addClass("bgMove");
+                    text.addClass("textMove");
+                    animationEnd(text, function () {
+                        text.find(".before").addClass("bgMove");
                     });
                 });
                 break;
@@ -38,6 +40,33 @@ var mySwiper = new Swiper('.swiper-container', {
         }
     }
 });
+
+var sildeOne = {
+    start: function () {
+
+    },
+    end: function () {
+
+    },
+};
+
+var sildeTwo = {
+    start: function () {
+
+    },
+    end: function () {
+
+    },
+};
+
+var sildeThree = {
+    start: function () {
+
+    },
+    end: function () {
+
+    },
+};
 
 var animationEnd = function (obj, callback) {
     obj.on("animationend webkitAnimationend", callback);
